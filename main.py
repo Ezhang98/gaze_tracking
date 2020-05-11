994e16bd544a2771180bc6ce43790090ba922045
Evan Zhang <evanlzhang@gmail.com>
2020-05-10 18:38:04 -0700

    trying add eye tracking

--- main.py
+++ main.py
@@ -25,10 +25,16 @@ while True:
     # Our operations on the frame come here
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    #x,y,w,h are the face positions
     for (x,y,w,h) in faces:
         frame = cv.rectangle(frame,(x,y),(x+w,y+h),(255,255,0),2)
-
-
+    gray_face = gray[y:y+h, x:x+w] # cut the gray face frame out
+    face = frame[y:y+h, x:x+w] # cut the face frame out
+    eyes = eye_cascade.detectMultiScale(gray_face)
+    #ex,ey,ew,eh are the eye positions
+    for (ex,ey,ew,eh) in eyes: 
+        cv.rectangle(frame,(ex,ey),(ex+ew,ey+eh),(0,225,255),2) 
+    
     # Display the resulting frame
     cv.imshow('frame', frame)
     if cv.waitKey(1) == ord('q'):
