427496809a2e12f3da865994cc441d8e09950e0d
Evan Zhang <evanlzhang@gmail.com>
2020-05-10 17:56:25 -0700

    face detection

--- main.py
+++ main.py
@@ -2,8 +2,38 @@
 # Martin Lopez
 
 
-import cv2
+import cv2 as cv
 import sys
 import numpy as np
 
+
+face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv.CascadeClassifier('haarcascade_eye.xml')
+
+
+cap = cv.VideoCapture(0)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    # Our operations on the frame come here
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        frame = cv.rectangle(frame,(x,y),(x+w,y+h),(255,255,0),2)
+
+
+    # Display the resulting frame
+    cv.imshow('frame', frame)
+    if cv.waitKey(1) == ord('q'):
+        break
+# When everything done, release the capture
+cap.release()
+cv.destroyAllWindows()
 
